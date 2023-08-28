fc8b611ef84ee4ee9769dcbe5e5e9560e5ad1ff7
SimpleLife <2239268289@qq.com>
2023-08-28 14:19:26 +0800

    'fixed'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withNextra = require('nextra')({
+  output: 'export',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
-  output: 'export',
 })
 
 module.exports = withNextra()
