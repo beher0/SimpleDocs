c96ba5f12357bc7746f684bcaa72dcaba2b707b2
SimpleLife <2239268289@qq.com>
2023-08-29 03:42:13 +0800

    'update'

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const repo = 'nextra-template'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
+  themeConfig: './theme.config.jsx',
 })
 
 const nextConfig = {
-  // assetPrefix: '/',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   output: 'export',
   images: {
     unoptimized: true,
