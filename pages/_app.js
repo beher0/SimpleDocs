39af252fcb530545c8f7358d74e92d1b9ce3e4b9
SimpleLife <2239268289@qq.com>
2023-08-29 04:08:38 +0800

    'fixed'

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../globalStyle.css';
-import {HashRouter} from 'react-router-dom'
 
 export default function MyApp({ Component, pageProps }) {
-  return <HashRouter>
-    <Component {...pageProps} />
-  </HashRouter>
+  return <Component {...pageProps} />
 }
