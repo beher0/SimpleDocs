13a34c45b7091d038a5b0e7495f388e28cc092cd
SimpleLife <2239268289@qq.com>
2023-09-01 00:06:58 +0800

    '修复图片路径'

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # nextra文档主题网站
 nextra框架搭建的文档主题网站
 https://simplelifecst.github.io/nextra-template
+
+# bug
+上线后图片存在问题，无法使用正确路径
--- components/Image/index.jsx
+++ components/Image/index.jsx
@@ -1,10 +1,21 @@
+import * as path from 'path';
 import style from './index.module.scss';
 
 function Image(props) {
   const {src, width, height} = props;
+
   return (
     <>
-      <img src={src} width={width} height={height} className={style.image} />
+      <img
+        src={
+          process.env.NODE_ENV != 'production'
+            ? src
+            : path.join('/nextra-template', src)
+        }
+        width={width}
+        height={height}
+        className={style.image}
+      />
     </>
   );
 }
