5460b3406d2a14460bf7278e1789401fbeac16cf
SimpleLife <2239268289@qq.com>
2023-09-05 23:38:50 +0800

    refactor

--- src/components/StartPage/index.jsx
+++ src/components/StartPage/index.jsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
-import style from './index.module.scss';
 
 export default function StartPage() {
   return (
-    <div className={style.container}>
-      <div className={style.title}>
+    <div className='start-page-container'>
+      <div className='title'>
         <header>使用Nextra框架和MDX语法打造优美的文档网站</header>
-        <div className={style.small_title}>
+        <div className='small_title'>
           简单、强大、灵活的站点生成框架 用Next. js中你喜欢的一切
         </div>
       </div>
 
-      <div className={style.btn}>
+      <div className='btn'>
         <Link href='/docs/instruction'>Get Started</Link>
         <span>→</span>
       </div>
