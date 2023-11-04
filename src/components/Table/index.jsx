2c92779bd1e240d7857b9c306f1b79a14f1de828
BeHer0 <dreamboy2239268289@gmail.com>
2023-11-04 21:06:57 +0800

    fix: 添加tailwind样式，对Table组件进行调整

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,36 +1,37 @@
-// todo
-export default function Table({list}) {
+import styles from './style.module.css';
+export default function Table({ list }) {
   return (
-    list && (
-      <table className='w-full border-collapse text-sm custom-table'>
+    <div
+      className={
+        '-mx-6 mb-4 mt-6 overflow-x-auto overscroll-x-contain px-6 pb-4 ' +
+        styles.container
+      }
+    >
+      <table className="w-full border-collapse text-sm">
         <thead>
-        <tr className='border-b py-4 text-left dark:border-neutral-700'>
-          <th className='py-2 font-semibold'>属性</th>
-          <th className='py-2 pl-6 font-semibold'>类型</th>
-          <th className='px-6 py-2 font-semibold'>描述</th>
-        </tr>
+          <tr className="border-b py-4 text-left dark:border-neutral-700">
+            <th className="py-2 font-semibold">属性</th>
+            <th className="py-2 pl-6 font-semibold">类型</th>
+            <th className="px-6 py-2 font-semibold">详情</th>
+          </tr>
         </thead>
-        <tbody className='align-baseline text-gray-900 dark:text-gray-100'>
-        {list.map(({option, type, desc = ''}, index) => {
-          return (
+        <tbody className="align-baseline text-gray-900 dark:text-gray-100">
+          {list.map(({ option, type, desc }) => (
             <tr
-              key={index}
-              className='border-b border-gray-100 dark:border-neutral-700/50'
+              key={option}
+              className="border-b border-gray-100 dark:border-neutral-700/50"
             >
-              <td
-                className='whitespace-pre py-2 font-mono text-xs font-semibold leading-6 text-violet-600 dark:text-violet-500'>
+              <td className="whitespace-pre py-2 font-mono text-xs font-semibold leading-6 text-violet-600 dark:text-violet-500">
                 {option}
               </td>
-              <td
-                className='whitespace-pre py-2 pl-6 font-mono text-xs font-semibold leading-6 text-slate-500 dark:text-slate-400'>
+              <td className="whitespace-pre py-2 pl-6 font-mono text-xs font-semibold leading-6 text-slate-500 dark:text-slate-400">
                 {type}
               </td>
-              <td className='py-2 pl-6'>{desc}</td>
+              <td className="py-2 pl-6">{desc}</td>
             </tr>
-          );
-        })}
+          ))}
         </tbody>
       </table>
-    )
+    </div>
   );
 }
