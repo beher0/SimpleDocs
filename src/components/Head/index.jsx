c3ce603f27220b3cd4b1a16da42507ca3a350324
BeHer0 <dreamboy2239268289@gmail.com>
2023-11-04 21:24:03 +0800

    refactor: 调整

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-      <meta property='og:title' content='SimpleLife'/>
-      <meta property='og:description' content='打造极简的文档主题'/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="SimpleLife" />
+      <meta property="og:description" content="打造极简的文档主题" />
     </>
   );
 }
